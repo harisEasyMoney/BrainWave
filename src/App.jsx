d0b25b7ce6a4eda53cbe662c4f994c31bae5a4ec
Haris Ahmad <haris341998@gmail.com>
2024-07-03 19:35:50 +0500

    Header and Mobile collapsable created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./Components/Button";
 import Header from "./Components/Header";
-
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-      </div>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+      <Header />
       <ButtonGradient />
     </>
   );
