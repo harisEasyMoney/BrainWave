c1af3593375b60dc01c4ef945f6d6bf7bdcbb52f
Haris Ahmad <haris341998@gmail.com>
2024-07-05 02:19:55 +0500

    "Created the pricing Portion"

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,8 +1,11 @@
-const Heading = ({ className, title, text }) => {
+import TagLine from "./TagLine";
+
+const Heading = ({ tag, className, title, text }) => {
   return (
     <div
       className={`${className} text-center max-w-[50rem] mx-auto mb-12 lg:mb-20`}
     >
+      {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
       {title && <h2 className="h2"> {title}</h2>}
       {text && <p className="text-center body-2 mt-4 text-n-4">{text}</p>}
     </div>
