b9003b8ba7b54d42f385ab4fe1c34017c79eea1c
Haris Ahmad <haris341998@gmail.com>
2024-07-04 18:55:59 +0500

    Created the benifits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benifits from "./Components/Benifits";
+import Collaboration from "./Components/Collaboration";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benifits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
