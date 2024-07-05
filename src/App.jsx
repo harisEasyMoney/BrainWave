f3223daad710d3f78e473c9a0d881b3e8c994c06
Haris Ahmad <haris341998@gmail.com>
2024-07-05 13:01:33 +0500

    Completed the Raodmap part

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./Components/Collaboration";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
 import Services from "./Components/Services";
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -5,7 +5,7 @@ const Heading = ({ tag, className, title, text }) => {
     <div
       className={`${className} text-center max-w-[50rem] mx-auto mb-12 lg:mb-20`}
     >
-      {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
+      {tag && <TagLine className="mb-4 justify-center ">{tag}</TagLine>}
       {title && <h2 className="h2"> {title}</h2>}
       {text && <p className="text-center body-2 mt-4 text-n-4">{text}</p>}
     </div>
